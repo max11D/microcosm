279bb6468050443ed248c711bc484e9a46edba1b
Maksim Mikityanskiy <qc@maksim.nyc>
2020-10-14 00:24:20 -0400

    initial build

---
+++ precache-manifest.964b34133791ee91825c94500102acec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f9aa87e12feb40ab4b45e992a9217f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cf68709ffdf91b37900",
+    "url": "/static/css/main.f2ec7647.chunk.css"
+  },
+  {
+    "revision": "755a7951509c20ecab02",
+    "url": "/static/js/2.f0d1fad7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f0d1fad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cf68709ffdf91b37900",
+    "url": "/static/js/main.80aea9ec.chunk.js"
+  },
+  {
+    "revision": "0104cf417331f6cf9ae78ba9c3cff5f6",
+    "url": "/static/js/main.80aea9ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04e3e1fe55b412c93adb",
+    "url": "/static/js/runtime-main.736d9e62.js"
+  }
+]);
